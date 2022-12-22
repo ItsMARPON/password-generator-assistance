a3b3cfa96923ee09dc00e5609ca20ff0f702283d
Mary <itsmaryyang@gmail.com>
2022-12-21 22:59:22 -0600

    Add more changes to JavaScript code

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,25 @@
 // Assignment code here
 
+
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
 
-let userInputName = 'Mary';
+let userInputName = '';
 userInputName ? console.log(`Hello ${userInputName}!`) :
 console.log('Hello!');
 
+const lowercasePassword = true;
+const uppercasePassword = true;
+const numericsPassword = true;
+const specialCharPassword = true;
+
+
+let lengthMinMax = prompt("What length of characters from 8 to 128?");
+let lowC = prompt("Do you want to include lowercase?");
+let upC= prompt("Do you want to include uppercase?");
+let numChars = prompt("Do you want to include numerics?");
+let specChars = prompt("Do you want to include special characters?");
+let passwordUserPrompt = console.log(lengthMinMax + lowC + upC + numChars + specChars);
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword(Math.floor(Math.random() * 128));
@@ -17,24 +30,6 @@ function writePassword() {
 }
 console.log(passwordText.value);
 
-switch(passwordText.value){
-  case 1:
-    console.log("What length of characters from 8 to 128?");
-    break;
-  case 2:
-    console.log("Do you want to include lowercase?");
-    break;
-  case 3:
-    console.log("Do you want to include uppercase?");
-    break;
-  case 4:
-    console.log("Do you want to include numerics?");
-    break;
-  case 5:
-    console.log("Do you want to include special characters?");
-    break;
-}
-console.log(passwordText.value);
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
