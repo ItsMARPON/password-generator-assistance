0a54bb4ffbe4fbeb86ea3ade36e6e1278d25814d
Mary <itsmaryyang@gmail.com>
2023-01-15 17:57:01 -0600

    Add code changes to JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,127 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
 
-let userInputName = '';
-userInputName ? console.log(`Hello ${userInputName}!`) :
-console.log('Hello!');
+const lowercasePassword = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const uppercasePassword = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const numericsPassword = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specialCharPassword = [
+  "+",
+  "-",
+  "&",
+  "|",
+  "!",
+  "(",
+  ")",
+  "{",
+  "}",
+  "[",
+  "]",
+  "^",
+  "~",
+  "*",
+  "?",
+  ":",
+];
+function generatePassword() {
+  var allChars = [];
+  let lengthMinMax = prompt("What length of characters from 8 to 128?");
+  while (lengthMinMax < 8 || lengthMinMax > 128) {
+    lengthMinMax = prompt(
+      "That is not valid. What length of characters from 8 to 128?"
+    );
+  }
+  let lowC = confirm("Do you want to include lowercase?");
+  if (lowC === true) {
+    allChars = allChars.concat(lowercasePassword);
+    console.log(allChars);
+  }
+  let upC = confirm("Do you want to include uppercase?");
+  if (upC === true) {
+    allChars = allChars.concat(uppercasePassword);
+    console.log(allChars);
+  }
+  let numChars = confirm("Do you want to include numerics?");
+  if (numChars === true) {
+    allChars = allChars.concat(numericsPassword);
+    console.log(allChars);
+  }
+  let specChars = confirm("Do you want to include special characters?");
+  if (specChars === true) {
+    allChars = allChars.concat(specialCharPassword);
+    console.log(allChars);
+  }
+  let password = "";
+  for (let i = 0; i < lengthMinMax; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password = password.concat(allChars[randomIndex]);
+  }
+  console.log(password);
+  return password;
+}
 
-const lowercasePassword = true;
-const uppercasePassword = true;
-const numericsPassword = true;
-const specialCharPassword = true;
-
-
-let lengthMinMax = prompt("What length of characters from 8 to 128?");
-let lowC = prompt("Do you want to include lowercase?");
-let upC= prompt("Do you want to include uppercase?");
-let numChars = prompt("Do you want to include numerics?");
-let specChars = prompt("Do you want to include special characters?");
-let passwordUserPrompt = console.log(lengthMinMax + lowC + upC + numChars + specChars);
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword(Math.floor(Math.random() * 128));
+  let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-console.log(passwordText.value);
-
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
-
+generateBtn.addEventListener("click", writePassword);
